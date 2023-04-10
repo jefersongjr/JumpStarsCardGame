fa3809243eaf51fae7120e0206bf8623d765883e
Jeferson Gomes <jeferson_gjr@hotmail.com>
2023-04-09 21:46:11 -0300

    :sparkles: feat: Criando o componente content

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
 import './App.css'
+import Content from './components/Content'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <p>Olá</p>
+      <Content />
     </div>
   )
 }
---
+++ app/src/components/Content.jsx
@@ -0,0 +1,9 @@
+function Content() {
+  return (
+    <div className="App">
+      <p>Criando o content</p>
+    </div>
+  )
+}
+
+export default Content
